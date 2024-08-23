9c831d78b69e840591fcdd50a7f86a1493681e9d
Dileshm <dileshmakanjee@hotmail.com>
2024-08-23 18:47:09 +0100

    First Draft

---
+++ GuessingGame.py
@@ -0,0 +1,19 @@
+# generate a number from 1 to 10
+# input from user
+# check if that input is a number 1 between 10
+# check if the number is the right guess
+# otherwise try again
+
+from random import randint
+
+while score == 0:
+    randint(1, 10)
+
+    print(input("Guess a number from 1 to 10: "))
+
+    if input == randint:
+        score.append += 1
+    elif:
+    print("Correct, would you like to play again? (yes/no)")
+    else:
+    randint = 0 and print("Incorrect, try again!")
