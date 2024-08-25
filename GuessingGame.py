1b6b7bbe057773c5df409b6ed952e2735a700f4e
Dileshm <dileshmakanjee@hotmail.com>
2024-08-25 17:22:08 +0100

    Update

--- GuessingGame.py
+++ GuessingGame.py
@@ -6,14 +6,39 @@
 
 from random import randint
 
-while score == 0:
-    randint(1, 10)
+lowest_num = 1
+highest_num = 100
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+is_running = True
 
-    print(input("Guess a number from 1 to 10: "))
+print("Guessing Game")
+print(f"Select a number between {lowest_num} and {highest_num}")
 
-    if input == randint:
-        score.append += 1
-    elif:
-    print("Correct, would you like to play again? (yes/no)")
+while is_running:
+
+    guess = input("Guess a number from 1 to 10: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_num or guess > highest_num:
+        print("Invalid guess, that number is out of range")
+        print(f"Please select a number between {lowest_num} and {highest_num}")
+    elif guess < answer:
+        print("You're under the number! Try again!")
+    elif guess < answer:
+        print("You're over the number! Try again!")
     else:
-    randint = 0 and print("Incorrect, try again!")
+        print("You're correct!, the answer is {answer}")
+        print(f"Your number of guesses were {guesses}")
+    else:
+print("Invalid guess, this is not a wordle game")
+print(f"Please select a number between {lowest_num} and {highest_num}")
+
+ is_running = False
+        play_again = input("Play again? (yes/no): ").lower()
+    if play_again != "yes":
+        print("Thank you for playing!")
+        break
