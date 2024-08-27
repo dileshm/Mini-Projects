31e5cfc3333d7452a98121d05954784f7bb60211
Dileshm <dileshmakanjee@hotmail.com>
2024-08-27 23:18:14 +0100

    Update

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,38 +1,46 @@
 from random import randint
 
 lowest_num = 1
-highest_num = 100
-answer = random.randint(lowest_num, highest_num)
-guesses = 0
-is_running = True
+highest_num = 10
 
-print("Guessing Game")
-print(f"Select a number between {lowest_num} and {highest_num}")
 
-while is_running:
+def play_game():
+    answer = randint(lowest_num, highest_num)
+    guesses = 0
+    is_running = True
 
-    guess = input("Guess a number from 1 to 10: ")
+    print("Guessing Game")
 
-    if guess.isdigit():
-        guess = int(guess)
-        guesses += 1
+    while is_running:
+        guess = input(f"Select a number between {
+                      lowest_num} and {highest_num}: ")
 
-        if guess < lowest_num or guess > highest_num:
-        print("Invalid guess, that number is out of range")
-        print(f"Please select a number between {lowest_num} and {highest_num}")
-    elif guess < answer:
-        print("You're under the number! Try again!")
-    elif guess < answer:
-        print("You're over the number! Try again!")
+        if guess.isdigit():
+            guess = int(guess)
+            guesses += 1
+
+            if guess < lowest_num or guess > highest_num:
+                print("Invalid guess, that number is out of range")
+                print(f"Please select a number between {
+                      lowest_num} and {highest_num}")
+            elif guess < answer:
+                print("You're under the number! Try again!")
+            elif guess > answer:
+                print("You're over the number! Try again!")
+            else:
+                print(f"You're correct! The answer is {answer}")
+                print(f"Your number of guesses was {guesses}")
+                is_running = False
+        else:
+            print("Invalid guess, this is not a number")
+            print(f"Please select a number between {
+                  lowest_num} and {highest_num}")
+
+    play_again = input("Play again? (yes/no): ").lower()
+    if play_again == "yes":
+        play_game()
     else:
-        print("You're correct!, the answer is {answer}")
-        print(f"Your number of guesses were {guesses}")
-    else:
-print("Invalid guess, this is not a wordle game")
-print(f"Please select a number between {lowest_num} and {highest_num}")
-
- is_running = False
-        play_again = input("Play again? (yes/no): ").lower()
-    if play_again != "yes":
         print("Thank you for playing!")
-        break
+
+
+play_game()
